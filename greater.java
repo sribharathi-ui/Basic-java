8a5be7e4fe84c908ac9325c83e5040ff320778d8
sribharathi-ui <sribharathil_bit27@mepcoeng.ac.in>
2026-02-27 11:10:58 +0530

    Add greater.java to compare two integers

---
+++ greater.java
@@ -0,0 +1,28 @@
+/**
+ * @author sri bharathi
+ */
+class If{
+   public static void main(String[] args)
+   {
+      /**
+       * @param integer
+       */
+      int x,y;
+      x = 10;
+      y = 20;
+      if(x < y)
+      {
+         System.out.println("x is lessthan y");
+      }
+      x = x*2;
+         if(x == y)
+         {
+            System.out.println("x is equal to y");
+         }
+      x = x*2;
+      if(x > y)
+      {
+         System.out.println("x is grater than y");
+      }
+   }
+}
